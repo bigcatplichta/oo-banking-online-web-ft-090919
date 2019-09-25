f364b20f5f7973610874bae403f20ce4d49aa2b5
Matt Plichta <bigcatplichta@gmail.com>
2019-09-25 18:18:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end 
   
   def execute_transaction
-    if self.valid?
+    if self.valid? && self.status == "pending"
       @sender.balance -= amount
       @receiver.deposit(amount)
       self.status = "complete"
